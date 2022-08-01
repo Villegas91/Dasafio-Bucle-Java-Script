b934d8e5b63121beab73964de3d112b4cdd74ec8
Victor Villegas <victoremanuelvillegas@gmail.com>
2022-07-31 22:22:17 -0300

    bucles

---
+++ main.js
@@ -0,0 +1,10 @@
+// Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una
+// salida por cada resultado
+// - Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida
+// por cada resultado, hasta que se ingresa “ESC”.
+// - Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces
+// ingresada.
+
+for (i = 0; i >= 20; i++) {
+  console.log(i);
+}
